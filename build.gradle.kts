ebf635031f64da5ee0f27d65527c02ece5e73f67
sya-ri <sya79lua@gmail.com>
2021-11-22 19:35:39 +0900

    chore: 依存関係のバージョンを更新

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,14 @@
 import dev.s7a.gradle.minecraft.server.tasks.LaunchMinecraftServerTask
+import dev.s7a.gradle.minecraft.server.tasks.LaunchMinecraftServerTask.JarUrl
 import groovy.lang.Closure
 import net.minecrell.pluginyml.bukkit.BukkitPluginDescription
 
 plugins {
     java
-    id("net.minecrell.plugin-yml.bukkit") version "0.4.0"
+    id("net.minecrell.plugin-yml.bukkit") version "0.5.0"
     id("com.github.ben-manes.versions") version "0.39.0"
     id("com.palantir.git-version") version "0.12.3"
-    id("dev.s7a.gradle.minecraft.server") version "1.0.2"
+    id("dev.s7a.gradle.minecraft.server") version "1.1.0"
 }
 
 val gitVersion: Closure<String> by extra
@@ -20,7 +21,7 @@ repositories {
 
 dependencies {
     implementation("org.spigotmc:spigot-api:1.17.1-R0.1-SNAPSHOT")
-    implementation("org.jetbrains:annotations:22.0.0")
+    implementation("org.jetbrains:annotations:23.0.0")
 }
 
 configure<BukkitPluginDescription> {
@@ -38,9 +39,9 @@ task<LaunchMinecraftServerTask>("buildAndLaunchServer") {
         }
     }
 
-    jarUrl.set("https://papermc.io/api/v1/paper/1.17.1/latest/download")
+    jarUrl.set(JarUrl.Paper("1.17.1"))
     jarName.set("server.jar")
     serverDirectory.set(buildDir.resolve("MinecraftServer"))
-    nogui.set(false)
+    nogui.set(true)
     agreeEula.set(true)
 }
